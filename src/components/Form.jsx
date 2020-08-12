23f2cf627941734ebe68c129b6d3822b2d848e40
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-12 19:08:20 +0200

    added styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,16 +16,16 @@ function Form(){
 <input type="text" name="firstName" placeholder="First Name " style={errors.firstName &&{borderColor: "red", backgroundColor: "lightpink"} } ref={register({required: true , maxLength: 20})} className="first-name input"/>
 <ErrorMessage error={errors.firstName}  />
 
-<input type="text" name="lastName" placeholder="Last Name" style={errors.firstName &&{borderColor: "red", backgroundColor: "lightpink"} } ref={register({required: true, })}className="last-name input"/>
+<input type="text" name="lastName" placeholder="Last Name" style={errors.lastName &&{borderColor: "red", backgroundColor: "lightpink"} } ref={register({required: true, })}className="last-name input"/>
 <ErrorMessage error={errors.lastName}  />
-<input type="email" name="email" placeholder="example@email.com" style={errors.firstName &&{borderColor: "red", backgroundColor: "lightpink"} } ref={register({required: true,pattern:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i})}className="email-address input"/>
+<input type="email" name="email" placeholder="example@email.com" style={errors.email &&{borderColor: "red", backgroundColor: "lightpink"} } ref={register({required: true,pattern:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i})}className="email-address input"/>
 <ErrorMessage error={errors.email}  />
 
-<input type="password" name="password" placeholder="Password" style={errors.firstName &&{borderColor: "red", backgroundColor: "lightpink"} } ref={register({required: true,maxLength:20} )}className="password input"/>
+<input type="password" name="password" placeholder="Password" style={errors.password &&{borderColor: "red", backgroundColor: "lightpink"} } ref={register({required: true,maxLength:20} )}className="password input"/>
 <ErrorMessage error={errors.password} />
     </form>
     <Button type="submit" onClick={handleSubmit(onSubmit)}> Claim your free trial</Button>
-
+    <h5 class="signup">By clicking the button you are agreeing to our <strong>Terms and Services</strong></h5>
     </Grid>
 </Grid>
     )
